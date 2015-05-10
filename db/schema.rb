7b65b7140e9a830dadf5486289dd86041a798b03
Your Name <you@example.com>
2015-05-10 14:50:35 +0300

    migr ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150510065128) do
+ActiveRecord::Schema.define(version: 20150510102023) do
 
   create_table "documents", force: true do |t|
     t.string   "title"
