57e85f77d6158c706d1ed2bed8f749ecab583038
Your Name <you@example.com>
2015-05-11 11:53:25 +0300

    create user model ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511082756) do
+ActiveRecord::Schema.define(version: 20150511085051) do
 
   create_table "documents", force: true do |t|
     t.string   "title"
@@ -24,6 +24,13 @@ ActiveRecord::Schema.define(version: 20150511082756) do
     t.string   "title"
     t.text     "body"
     t.string   "image_url"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
