4334c884fe1441be0795b956b20984fb994b438c
Your Name <you@example.com>
2015-05-11 12:32:48 +0300

    validations user ok

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150511085051) do
+ActiveRecord::Schema.define(version: 20150511092953) do
 
   create_table "documents", force: true do |t|
     t.string   "title"
@@ -34,5 +34,7 @@ ActiveRecord::Schema.define(version: 20150511085051) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
