4ef148cac147564533cf14fbdf3aec3b9b467d71
Your Name <you@example.com>
2015-05-17 11:12:09 +0300

    failed edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,22 @@ class UsersController < ApplicationController
 			end
 	end	
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      #
+    else
+      flash[:success] = "no valid form!"
+      render 'edit'
+    end
+  end
+
 	private
+
 		def	user_params
 			params.require(:user).permit(:name,	:email,	:password, :password_confirmation)
 		end	
