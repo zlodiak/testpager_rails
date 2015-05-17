e07b7464d7d7b11cedd9ce8afd1c0ed477f7be1d
Your Name <you@example.com>
2015-05-17 11:35:27 +0300

    signin check ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
 	def index 
 		@user	=	User.all
 	end
@@ -42,4 +43,8 @@ class UsersController < ApplicationController
 		def	user_params
 			params.require(:user).permit(:name,	:email,	:password, :password_confirmation)
 		end	
+
+    def signed_in_user 
+      redirect_to signin_url, notice: 'please signed in' unless signed_in?
+    end
 end
