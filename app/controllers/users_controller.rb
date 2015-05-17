29c11535092fc7eac00e919653db7938b9d41819
Your Name <you@example.com>
2015-05-17 20:52:18 +0300

    redirects new ok

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -47,7 +47,10 @@ class UsersController < ApplicationController
 		end	
 
     def signed_in_user 
-      redirect_to signin_url, notice: 'please signed in' unless signed_in?
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: 'please signed in'
+      end
     end
 
     def correct_user
